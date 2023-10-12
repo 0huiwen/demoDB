1d5e06a526fcd66ca74d8a3f7a7ce9a6c54b754a
0huiwen <142558329+0huiwen@users.noreply.github.com>
2023-10-12 10:18:37 +0800

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,13 +1,16 @@
-<?php 
-define('DB_HOST', '20.212.177.3:3306'); // Remove any spaces between the IP and port
+<?php
+
+define('DB_HOST', '20.205.163.45');
 define('DB_USER', 'root');
-define('DB_PASS', 'Demouser_2023');
+define('DB_PASS', 'Demouser2023');
 define('DB_NAME', 'demoDBschema');
 
-// Establish database connection.
-try {
-    $dbh = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
-} catch (PDOException $e) {
-    exit("Error: " . $e->getMessage());
+try
+{
+$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array($
+}
+catch (PDOException $e)
+{
+exit("Error: " . $e->getMessage());
 }
 ?>
